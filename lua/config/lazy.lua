279fd9f4bd41f50afb12f348b35da5d37b4690c7
NaiKiDEV <pc999111@gmail.com>
2024-12-11 23:39:09 +0200

    feat: add oil, snacks and configure some options

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,12 +12,14 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     os.exit(1)
   end
 end
+
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   spec = {
-    -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+
+    -- Forced theme (Gruvbox or Github Dark)
     {
       "sainnhe/gruvbox-material",
       lazy = false,
@@ -30,7 +32,8 @@ require("lazy").setup({
         vim.cmd.colorscheme("gruvbox-material")
       end,
     },
-    --[[ Maybe github_dark_default looks fine?
+
+    --[[
     {
       "projekt0n/github-nvim-theme",
       lazy = false, -- make sure we load this during startup if it is your main colorscheme
@@ -43,7 +46,175 @@ require("lazy").setup({
       end,
     },
     --]]
-    -- import/override with your plugins
+
+    {
+      "folke/snacks.nvim",
+      priority = 1000,
+      lazy = false,
+      ---@type snacks.Config
+      opts = {
+        bigfile = { enabled = true },
+        dashboard = {
+          enabled = true,
+          sections = {
+            { section = "header" },
+            { section = "keys", gap = 0 },
+            { icon = "> ", title = "Projects", section = "projects", indent = 2, padding = 4 },
+            { section = "startup" },
+          },
+        },
+        git = { enabled = false },
+        gitbrowse = { enabled = false },
+        indent = { enabled = true },
+        input = { enabled = true },
+        notifier = {
+          enabled = true,
+          timeout = 3000,
+        },
+        quickfile = { enabled = true },
+        scroll = { enabled = true },
+        statuscolumn = { enabled = true },
+        words = { enabled = true },
+        styles = {
+          notification = {},
+        },
+        zen = { enabled = false },
+      },
+      keys = {
+        {
+          "<leader>.",
+          function()
+            Snacks.scratch()
+          end,
+          desc = "Toggle Scratch Buffer",
+        },
+        {
+          "<leader>S",
+          function()
+            Snacks.scratch.select()
+          end,
+          desc = "Select Scratch Buffer",
+        },
+        {
+          "<leader>n",
+          function()
+            Snacks.notifier.show_history()
+          end,
+          desc = "Notification History",
+        },
+        {
+          "<leader>bd",
+          function()
+            Snacks.bufdelete()
+          end,
+          desc = "Delete Buffer",
+        },
+        {
+          "<leader>cR",
+          function()
+            Snacks.rename.rename_file()
+          end,
+          desc = "Rename File",
+        },
+        {
+          "<leader>gg",
+          function()
+            Snacks.lazygit()
+          end,
+          desc = "Lazygit",
+        },
+        {
+          "<leader>gl",
+          function()
+            Snacks.lazygit.log()
+          end,
+          desc = "Lazygit Log (cwd)",
+        },
+        {
+          "<leader>un",
+          function()
+            Snacks.notifier.hide()
+          end,
+          desc = "Dismiss All Notifications",
+        },
+        {
+          "<c-/>",
+          function()
+            Snacks.terminal()
+          end,
+          desc = "Toggle Terminal",
+        },
+        {
+          "]]",
+          function()
+            Snacks.words.jump(vim.v.count1)
+          end,
+          desc = "Next Reference",
+          mode = { "n", "t" },
+        },
+        {
+          "[[",
+          function()
+            Snacks.words.jump(-vim.v.count1)
+          end,
+          desc = "Prev Reference",
+          mode = { "n", "t" },
+        },
+      },
+    },
+
+    {
+      "nvim-treesitter/nvim-treesitter",
+      opts = {
+        ensure_installed = {
+          "bash",
+          "html",
+          "javascript",
+          "json",
+          "lua",
+          "markdown",
+          "markdown_inline",
+          "python",
+          "query",
+          "regex",
+          "tsx",
+          "typescript",
+          "vim",
+          "yaml",
+        },
+      },
+    },
+
+    {
+      "nvim-treesitter/nvim-treesitter",
+      opts = function(_, opts)
+        vim.list_extend(opts.ensure_installed, {
+          "go",
+          "tsx",
+          "typescript",
+        })
+      end,
+    },
+
+    {
+      "stevearc/oil.nvim",
+      ---@module 'oil'
+      ---@type oil.SetupOpts
+      opts = {
+        view_options = {
+          show_hidden = true,
+        },
+      },
+      -- Optional dependencies
+      dependencies = { { "echasnovski/mini.icons", opts = {} } },
+      -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+    },
+
+    {
+      "folke/trouble.nvim",
+      opts = { use_diagnostic_signs = true },
+    },
+
     { import = "plugins" },
   },
   defaults = {
@@ -61,7 +232,6 @@ require("lazy").setup({
         "gzip",
         "tarPlugin",
         "tohtml",
-        "tutor",
         "zipPlugin",
       },
     },
